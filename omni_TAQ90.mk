b32f692faf21a8d131f22b3525256f93d71003ca
Tommy0412 <37461047+Tommy0412@users.noreply.github.com>
2026-05-31 02:39:38 +0200

    Update omni_TAQ90.mk

--- omni_TAQ90.mk
+++ omni_TAQ90.mk
@@ -4,11 +4,11 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, bootable/recovery/twrp.mk)
 
 # Inherit from TAQ90 device
 $(call inherit-product, device/denver/TAQ90/device.mk)
